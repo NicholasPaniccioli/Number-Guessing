6584e582c76e89ebc26d1ff37ded31a22924572e
Nicholas Paniccioli <nicholaspanic20@gmail.com>
2025-05-16 19:49:49 -0400

    Outline started
    Began work on my guessing game, started with writing out an outline of what I believe needs to happen to get a working program.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Number-Guessing
 My Number Guessing Game submission for the Roadmap projects
+
+Project page can be found [here](https://roadmap.sh/projects/number-guessing-game)
---
+++ number-guess.py
@@ -0,0 +1,10 @@
+#Welcome player to the game
+print("Welcome to the Number Guessing Game!" \
+"\nA random number between 1 and 100 will be chosen." \
+"\nTry to guess the number within a few guesses. Goodluck!")
+
+#Showcase the difficulty levels & take user selection
+
+#Start the game/loop till guesses hit 0 
+#or correct guess is given.
+
