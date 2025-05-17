1b50bd9982c9ac175ebd48912fec85c168bee24f
Nicholas Paniccioli <nicholaspanic20@gmail.com>
2025-05-17 18:00:20 -0400

    Bare Minimum Game Complete
    Went through and built out the loop for the main game. Player can now play a game and the program will end when the game ends.
    
    Still need to add some more functionality and checks for wrong responses.

--- number-guess.py
+++ number-guess.py
@@ -1,3 +1,16 @@
+import random
+
+#Helper methods
+def start_game(chances):
+    global user_chances
+    global rand_num
+    global attempts
+    user_chances = chances
+    rand_num = random.randrange(1,100)
+    attempts = 0
+    print("\nGame has started! Good luck!")
+
+
 #Welcome player to the game
 print("Welcome to the Number Guessing Game!" \
 "\nA random number between 1 and 100 will be chosen." \
@@ -8,16 +21,39 @@ print("\nSelect the difficulty level:" \
 "\n1. Easy (10 chances)" \
 "\n2. Medium (5 chances)" \
 "\n3. Hard (3 chances)")
-
 user_input = input("\nEnter your choice: ")
 
-#Start the game/loop till guesses hit 0 
-#or correct guess is given.
+#Set Players chances and start game
 if user_input == "1":
     print("\nGreat! You selected Easy. You have 10 chances!")
+    start_game(10)
 elif user_input == "2":
     print("\nGreat! You selected Medium. You have 5 chances!")
+    start_game(5)
 elif user_input == "3":
     print("\nGreat! You selected Hard. You have 3 chances!")
+    start_game(3)
+    print(rand_num)
 else:
     print("You did not select a valid difficulty.")
+
+#loops until game is over
+while user_chances > 0:
+    user_guess = input("Enter your guess: ")
+    if user_guess != str(rand_num) and int(user_guess) > rand_num:
+        print("Incorrect! The number is less than " + user_guess)
+        user_chances -= 1
+        attempts += 1
+        if user_chances == 0:
+            print("Nice try, unfortunately you did not guess the number.")
+    elif user_guess != str(rand_num) and int(user_guess) < rand_num:
+        print("Incorrect! The number is greater than " + user_guess)
+        user_chances -= 1
+        attempts += 1
+
+        if user_chances == 0:
+            print("Nice try, unfortunately you did not guess the number.")
+    else:
+        attempts += 1
+        print("Congrats! You guessed the correct number in " + str(attempts) + " attempts." )
+        break
