7800a6dbbeca6635deb765177740af0b876966bb
Nicholas Paniccioli <nicholaspanic20@gmail.com>
2025-05-16 20:11:05 -0400

    Difficulty Selection Added
    Added more descriptive text for the game. Player can now 'select' a difficulty and be given the proper response.

--- number-guess.py
+++ number-guess.py
@@ -4,7 +4,20 @@ print("Welcome to the Number Guessing Game!" \
 "\nTry to guess the number within a few guesses. Goodluck!")
 
 #Showcase the difficulty levels & take user selection
+print("\nSelect the difficulty level:" \
+"\n1. Easy (10 chances)" \
+"\n2. Medium (5 chances)" \
+"\n3. Hard (3 chances)")
+
+user_input = input("\nEnter your choice: ")
 
 #Start the game/loop till guesses hit 0 
 #or correct guess is given.
-
+if user_input == "1":
+    print("\nGreat! You selected Easy. You have 10 chances!")
+elif user_input == "2":
+    print("\nGreat! You selected Medium. You have 5 chances!")
+elif user_input == "3":
+    print("\nGreat! You selected Hard. You have 3 chances!")
+else:
+    print("You did not select a valid difficulty.")
